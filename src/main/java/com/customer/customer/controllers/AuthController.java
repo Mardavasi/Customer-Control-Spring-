d2c1451f1c9c857bb496380a78742d1f6f948539
marlon <md-vasquezs@corhuila.edu.co>
2024-06-24 09:35:59 -0500

    Implementacion de libreria Auth0 para generar token y control de seccion

--- src/main/java/com/customer/customer/controllers/AuthController.java
+++ src/main/java/com/customer/customer/controllers/AuthController.java
@@ -4,6 +4,7 @@ package com.customer.customer.controllers;
 import com.customer.customer.dto.RequestLogin;
 import com.customer.customer.entities.User;
 import com.customer.customer.services.AuthService;
+import com.customer.customer.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +17,12 @@ public class AuthController {
     private AuthService service;
 
     @PostMapping("/auth/login")
-    public User login (@RequestBody RequestLogin request){
+    public String login (@RequestBody RequestLogin request){
         String email = request.getEmail();
-        String password = request.getEmail();
+        String password = request.getPassword();
         User user = service.login(email,password);
-        return user;
+        String token = JwtUtil.generateToken(user);
+        return token;
 
     }
 }
